6f8276aad509c6964d217002099b38155316c335
LouiseDurandJanin <louisedurandjanin@gmail.com>
2024-02-04 21:16:01 +0100

    Update labels.py
    remove unnecessary comments

--- labels.py
+++ labels.py
@@ -1,17 +1,11 @@
 import torch
 import numpy as np
-from sklearn.cluster import MiniBatchKMeans
 from k_means_constrained import KMeansConstrained
 
 
-data = torch.load('/home/louisedurand-janin/Public/text_embeddings.pt')
-#data = torch.load('/Users/elsaazoulay/Downloads/text_embeddings.pt')
-
-#labels = MiniBatchKMeans(n_clusters=825, init='k-means++').fit_predict(data)
-#np.save('/Users/elsaazoulay/Downloads/labels.npy', labels)
+data = torch.load('./text_embeddings.pt')
 
 clf = KMeansConstrained(n_clusters=512, size_min=32, random_state=0)
 labels = clf.fit_predict(data)
 
-#np.save('/Users/elsaazoulay/Downloads/labels.npy', labels)
-np.save('/home/louisedurand-janin/Public/labels.npy', labels)
+np.save('./labels.npy', labels)
